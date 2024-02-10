32c01f6c8a7145733e90007db57418e86ab2c213
kengo-taka <1117marine1117@gmail.com>
2024-02-10 16:26:21 +0900

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article; 
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+ $articles = \Auth::user()->articles()->orderBy('created_at', 'desc')->paginate(10);
+ $data = [
+     'articles' => $articles,
+ ];
+ return view('home', $data); }
+}
